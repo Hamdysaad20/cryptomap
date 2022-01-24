b404a055f4f57cdcb420fbe859462523d4817c9b
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-01-25 01:49:13 +0200

    l

--- components/NavnonavsBar.js
+++ components/NavnonavsBar.js
@@ -1,12 +1,11 @@
-import Sininout from "../components/Sininout";
 
 function NavnonavsBar(props) {
 
     return (
         <div className="mt-[-72px] pb-1  block sm:hidden">
-                   <header className=" bg-gray-900 pt-7 text-center overflow-hidden lg:max-w-9xl sticky top-0 z-50 grid grid-cols-2   shadow-sm ">
+                   <header className=" bg-gray-900 pt-7 text-center overflow-hidden lg:max-w-9xl sticky top-0 z-50    shadow-sm ">
            
-           <div style={{color: 'white', fontWeight: 800}}  className=" text-left md:text-center ml-3 sm:ml-7 text-2xl md:text-3xl py-auto  md:pt-0 pt-1">
+           <div style={{color: 'white', fontWeight: 800}}  className="mt-2 text-center md:text-center  text-2xl md:text-3xl   md:pt-0 ">
                <div style={{fontFamily:"BalooBhaijaan2",fontWeight:"800" }} className=" pb-3">
                    Crypto Map
                </div>
@@ -14,9 +13,7 @@ function NavnonavsBar(props) {
          
          
            <div className=" pb-1 text-right mr-6 ">
-           <Sininout
-           textcolor="text-white"
-           />
+
 
           
 
