baa339300dbcdaa5740bb5b902fb2c2f496f33ae
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-01-28 17:20:36 +0200

    ads

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,11 @@ export default function Home() {
     <div className='relative h-screen '>
       <Head>
         <title>CryptoMap</title>
+        <script
+          async
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3704254540700382'
+          crossorigin='anonymous'></script>
+
         <meta
           name='description'
           content='منصة عربية لمحبين العملات الرقمية والثقافة المالية والاخبار الجديدة في سوق المال العالمي مجمعة من جميع انحاء العالم في مصدر واحد وباللغة العربية cryptomap كريبتوماب هو صديقك المالي ومهمتنا ان نجعل الثقافة المالية ممتعة وتركيزنا يكون اكثر عن العملات الرقمية و nfts والربح منها وكيف تعمل '
